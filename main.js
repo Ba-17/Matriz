672b751dc4a1dcc304f7cde123d794dd36a5f313
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-09-27 11:58:48 -0300

    t

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ alert(nomeUsuario)
 function soma (número1, número2) {
     return número1+número2;
 }
-
 console.log(soma(6,4));
 
 function areaQuadrado (lado) {
     return lado**2;
 }
-
 console.log(areaQuadrado(6));
+
+let media=function (número1,número2,número3,número4){
+    return número1+número2+número3/número4;
+}
+
+console.log(soma(1,2,3.3));
