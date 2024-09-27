9e20fa720feb801541a3047c2d497e8d9c0b4723
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-09-27 12:14:09 -0300

    u

--- main.js
+++ main.js
@@ -15,3 +15,6 @@ let media=function (número1,número2,número3){
     return (número1+número2+número3)/3;
 }
 console.log(media(1,2,3));
+
+let nome=parseInt(prompt(qual foi sua nota no 1°trimestre?))
+document.write(nome);
