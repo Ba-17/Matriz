f5844341eb52de1c6aefb2f57a66bcc4ce5afd61
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-10-04 11:26:44 -0300

    g

--- main.js
+++ main.js
@@ -13,19 +13,19 @@
 
 
 
-let media=function (número1,número2,número3){
-    return (número1+número2+número3)/3;
-}
-console.log(media(1,2,3));
+// let media=function (número1,número2,número3){
+//     return (número1+número2+número3)/3;
+// }
+// console.log(media(1,2,3));
 
-let número1= parseInt (prompt ('Qual o n°?'));
-let número2= parseInt (prompt ('Qual o n°?'));
-let número3= parseInt (prompt ('Qual o n°?'));
+// let número1= parseInt (prompt ('Qual o n°?'));
+// let número2= parseInt (prompt ('Qual o n°?'));
+// let número3= parseInt (prompt ('Qual o n°?'));
 
-function media (x,y,z){
-return (x+y+z)/3;
-}
-document.write (media(número1, número2, número3));
+// function media (x,y,z){
+// return (x+y+z)/3;
+// }
+// document.write (media(número1, número2, número3));
 
 let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
 
