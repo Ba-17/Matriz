813d5797d07e16866f8881c8e025d047b26ddc54
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-10-04 11:22:49 -0300

    h

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-let nomeUsuario='Ana,Bárbara,Michel';
-alert(nomeUsuario)
+// let nomeUsuario='Ana,Bárbara,Michel';
+// alert(nomeUsuario)
 
-function soma (número1, número2) {
-    return número1+número2;
-}
-console.log(soma(6,4));
+// function soma (número1, número2) {
+//     return número1+número2;
+// }
+// console.log(soma(6,4));
 
-function areaQuadrado (lado) {
-    return lado**2;
-}
-console.log(areaQuadrado(6));
+// function areaQuadrado (lado) {
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
+
+
 
 let media=function (número1,número2,número3){
     return (número1+número2+número3)/3;
 }
 console.log(media(1,2,3));
 
-let nome=parseInt(prompt('qual foi sua nota nos 3 trimestres?'));
-document.write(nome);
+let número1= parseInt (prompt ('Qual o n°?'));
+let número2= parseInt (prompt ('Qual o n°?'));
+let número3= parseInt (prompt ('Qual o n°?'));
+
+function media (x,y,z){
+return (x+y+z)/3;
+}
+document.write (media(número1, número2, número3));
+
+let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
 
